b7e67e931f1c89c988c46c07866cbc2896d1027b
Catalin Leca <leca.catalin1995@gmail.com>
2026-01-20 14:48:07 +0100

    implement author preview url editor

--- apps/frontend/editor/src/api/Hunt/sharing/index.ts
+++ apps/frontend/editor/src/api/Hunt/sharing/index.ts
@@ -1,4 +1,5 @@
 export { useGetPlayerInvitations } from './getPlayerInvitations';
+export { useGetPreviewLink } from './getPreviewLink';
 export { useInvitePlayer } from './invitePlayer';
 export { useRevokeInvitation } from './revokeInvitation';
 export { useResetPlayLink } from './resetPlayLink';
