627a6b85e149cc0d4dcec51aa41b8f21aa8ec2a0
Catalin Leca <leca.catalin1995@gmail.com>
2026-01-20 22:29:39 +0100

    hide dev routes

--- apps/frontend/player/src/pages/PreviewPage/components/index.ts
+++ apps/frontend/player/src/pages/PreviewPage/components/index.ts
@@ -1,4 +1,3 @@
 export { EmbeddedPreview } from './EmbeddedPreview';
 export { PreviewContent } from './PreviewContent';
 export { PreviewToolbar } from './PreviewToolbar';
-export { HuntPicker } from './HuntPicker';
