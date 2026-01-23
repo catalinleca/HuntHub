bd1879edea34922d1b3480ecf0016c061d59366e
Catalin Leca <leca.catalin1995@gmail.com>
2026-01-23 15:19:34 +0100

    login page

--- apps/frontend/editor/src/pages/LoginPage/components/LoginHeader.tsx
+++ apps/frontend/editor/src/pages/LoginPage/components/LoginHeader.tsx
@@ -1,12 +1,12 @@
 import { Stack, Typography } from '@mui/material';
-import { MapPinIcon } from '@phosphor-icons/react';
+import { CompassIcon } from '@phosphor-icons/react';
 import { getColor } from '@hunthub/compass';
 import * as S from '../LoginPage.styles';
 
 export const LoginHeader = () => (
   <Stack alignItems="center" gap={3}>
     <S.IconCircle alignItems="center" justifyContent="center">
-      <MapPinIcon size={32} weight="fill" color={getColor('common.white')} />
+      <CompassIcon size={32} weight="fill" color={getColor('common.white')} />
     </S.IconCircle>
     <Stack alignItems="center" gap={1}>
       <Typography variant="h4" fontWeight={700}>
